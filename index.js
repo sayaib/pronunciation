8daef347a001bb99a0c47efbd40f1fa1544469c6
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2020-11-21 16:06:19 +0530

    update

---
+++ index.js
@@ -0,0 +1,152 @@
+// ********************************** Number Pronunciation Code **************************
+$(".zero").on("click", function () {
+    var audio = new Audio("number/0.mp3");
+    audio.play();
+});
+$(".one").on("click", function () {
+    var audio = new Audio("number/1.mp3");
+    audio.play();
+});
+$(".two").on("click", function () {
+    var audio = new Audio("number/2.mp3");
+    audio.play();
+});
+$(".three").on("click", function () {
+    var audio = new Audio("number/3.mp3");
+    audio.play();
+});
+$(".four").on("click", function () {
+    var audio = new Audio("number/4.mp3");
+    audio.play();
+});
+$(".five").on("click", function () {
+    var audio = new Audio("number/5.mp3");
+    audio.play();
+});
+$(".six").on("click", function () {
+    var audio = new Audio("number/6.mp3");
+    audio.play();
+});
+$(".seven").on("click", function () {
+    var audio = new Audio("number/7.mp3");
+    audio.play();
+});
+$(".eight").on("click", function () {
+    var audio = new Audio("number/8.mp3");
+    audio.play();
+});
+$(".nine").on("click", function () {
+    var audio = new Audio("number/9.mp3");
+    audio.play();
+});
+$(".ten").on("click", function () {
+    var audio = new Audio("number/10.mp3");
+    audio.play();
+});
+
+// ********************************* alphabate Section ******************************
+
+$(".a").on("click", function(){
+  var audio = new Audio("alphabate/a.mp3");
+  audio.play();
+});
+$(".b").on("click", function(){
+  var audio = new Audio("alphabate/b.mp3");
+  audio.play();
+});
+$(".c").on("click", function(){
+  var audio = new Audio("alphabate/c.mp3");
+  audio.play();
+});
+$(".d").on("click", function(){
+  var audio = new Audio("alphabate/d.mp3");
+  audio.play();
+});
+$(".e").on("click", function(){
+  var audio = new Audio("alphabate/e.mp3");
+  audio.play();
+});
+$(".f").on("click", function(){
+  var audio = new Audio("alphabate/f.mp3");
+  audio.play();
+});
+$(".g").on("click", function(){
+  var audio = new Audio("alphabate/g.mp3");
+  audio.play();
+});
+$(".h").on("click", function(){
+  var audio = new Audio("alphabate/h.mp3");
+  audio.play();
+});
+$(".i").on("click", function(){
+  var audio = new Audio("alphabate/i.mp3");
+  audio.play();
+});
+$(".j").on("click", function(){
+  var audio = new Audio("alphabate/j.mp3");
+  audio.play();
+});
+$(".k").on("click", function(){
+  var audio = new Audio("alphabate/k.mp3");
+  audio.play();
+});
+$(".l").on("click", function(){
+  var audio = new Audio("alphabate/l.mp3");
+  audio.play();
+});
+$(".m").on("click", function(){
+  var audio = new Audio("alphabate/m.mp3");
+  audio.play();
+});
+$(".n").on("click", function(){
+  var audio = new Audio("alphabate/n.mp3");
+  audio.play();
+});
+$(".o").on("click", function(){
+  var audio = new Audio("alphabate/o.mp3");
+  audio.play();
+});
+$(".p").on("click", function(){
+  var audio = new Audio("alphabate/p.mp3");
+  audio.play();
+});
+$(".q").on("click", function(){
+  var audio = new Audio("alphabate/q.mp3");
+  audio.play();
+});
+$(".r").on("click", function(){
+  var audio = new Audio("alphabate/r.mp3");
+  audio.play();
+});
+$(".s").on("click", function(){
+  var audio = new Audio("alphabate/s.mp3");
+  audio.play();
+});
+$(".t").on("click", function(){
+  var audio = new Audio("alphabate/t.mp3");
+  audio.play();
+});
+$(".u").on("click", function(){
+  var audio = new Audio("alphabate/u.mp3");
+  audio.play();
+});
+$(".v").on("click", function(){
+  var audio = new Audio("alphabate/v.mp3");
+  audio.play();
+});
+$(".w").on("click", function(){
+  var audio = new Audio("alphabate/w.mp3");
+  audio.play();
+});
+$(".x").on("click", function(){
+  var audio = new Audio("alphabate/x.mp3");
+  audio.play();
+});
+$(".y").on("click", function(){
+  var audio = new Audio("alphabate/y.mp3");
+  audio.play();
+});
+$(".z").on("click", function(){
+  var audio = new Audio("alphabate/z.mp3");
+  audio.play();
+});
